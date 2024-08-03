7433b0f3dff6d0ba5aea9f3477820e44ef7ceced
d3vmeh <devxmehra@gmail.com>
2024-08-03 12:33:11 -0700

    Removing old prompt

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ from langchain_core.output_parsers import StrOutputParser
 from create_database import *
 
 
-
-def get_response(query,context,prompt,llm):
-
+def get_response(query,context,llm):
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in context])
-    print(context_text)
-    prompt_template = ChatPromptTemplate.from_template(prompt)
-    prompt = prompt_template.format(context=context_text, question=query)
-
-    
+    #print(context_text)
     prompt = ChatPromptTemplate.from_messages(
         [
         (
@@ -47,38 +41,19 @@ def get_response(query,context,prompt,llm):
 
 embeddings = OpenAIEmbeddings()
 
+
+"""
+Run to Create/Update Chrome DB
+"""
 #chunks = create_chunks(replace_newlines=True)
 #print(len(chunks))
 #save_database(embeddings,chunks,path="standard-rag-foreign-policy/Chroma")
 
 db = load_database(embeddings,path="standard-rag-foreign-policy/Chroma")
-
 llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0.7)
 
 while True:
     query = input("Enter a question: ")
-    results = query_database(query, db, num_responses = 20)
-
-
-    prompt = """
-    Answer the question only based on the following context:
-
-
-
-    Here is the context you can use to help you answer the questions:
-    {context}
-
-
-
-    ------------
-
-
-    If you do not know the answer, do not make up an answer, just say you do not know. 
-    
-    Answer the question based on the above context. Here is the question:
-    
-    {question}"""
-
-    response, response_text = get_response(query, results, prompt, llm)
-
+    results = query_database(query, db, num_responses = 100)
+    response, response_text = get_response(query, results, llm)
     print(f"\n{response}\n\n\n-----------------------------------------------------------------")
